ec0daf4dcff10bc2514341f3861ec91bd1b629a3
thierno sadou diallo <thierved01@gmail.com>
2019-08-11 18:41:18 +0000

    added the remaining routes

--- models/index.js
+++ models/index.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = Promise;
 
-mongoose.connect('mongodb://localhost:27017/todo');
+mongoose.connect('mongodb://localhost:27017/todo', { useNewUrlParser: true });
 
 module.exports = { mongoose };
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -4,12 +4,8 @@ const {Todo} = require('../models/todos');
 
 router.get('/', (req, res) => {
     Todo.find()
-        .then(todo => {
-            res.json(todo);
-        })
-        .catch(err => {
-            res.send(err);
-        });
+        .then(todo => res.json(todo))
+        .catch(err => res.send(err));
 });
 
 router.post('/', (req, res) => {
@@ -17,5 +13,23 @@ router.post('/', (req, res) => {
         .then(todo => res.json(todo))
         .catch(err => res.send(err));
 });
+
+router.get('/:todoId', (req, res) => {
+    Todo.findById(req.params.todoId)
+        .then(todo => res.json(todo))
+        .catch(e => res.send(e));
+});
+
+router.put('/:todoId', (req, res) => {
+    Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+        .then(todo => res.json(todo))
+        .catch(e => res.send(e));
+});
+
+router.delete('/:todoId', (req, res) => {
+    Todo.remove({_id: req.params.todoId})
+        .then(todo => res.send({message: "todo deleted successfully!"}))
+        .catch(e => res.send(e));
+})
 
 module.exports = router;
