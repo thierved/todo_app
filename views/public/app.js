5dbdc6bba2c3c132e716c13447e2e43d1c5c8abf
thierno sadou diallo <thierved01@gmail.com>
2019-08-19 18:33:52 +0000

    added the add todo functionality

--- views/public/app.js
+++ views/public/app.js
@@ -1,10 +1,34 @@
 const todoList = document.getElementById('todo-list');
+const input = document.getElementById('input');
 
 fetch("http://localhost:3000/api/todos", {method: "GET"})
     .then(res => res.json())
     .then(todos => {
         for (const todo of todos) {
-            const todoItem = `<li>${todo.name}</li>`;
-            todoList.innerHTML += todoItem;
+            postTodo(todo);   
         }
     });
+
+input.addEventListener('keypress', e => {
+    if(e.keyCode === 13) {
+        const todo = input.value;
+        (async () => {
+            const rawResponse = await fetch('http://localhost:3000/api/todos', {
+              method: 'POST',
+              headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({"name": todo})
+            });
+            const newTodo = await rawResponse.json();
+            postTodo(newTodo);
+        })();
+    
+    }
+});
+
+function postTodo(todo) {
+    const todoItem = `<li>${todo.name}</li>`;
+    todoList.innerHTML += todoItem;
+}
